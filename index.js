0d7146ec3084a0c7bbf1194a29b50c1203475879
Imran-Siddiquii <siddiquiimran132@gmail.com>
2022-09-04 16:23:18 +0530

    fact: style:

--- index.js
+++ index.js
@@ -4,26 +4,46 @@ const checkButton=document.querySelector("#check-button");
 const errorMessage=document.querySelector(".error-show");
 
 const noOfNotes=document.querySelectorAll(".noOfNote");
+const inputTwo=document.querySelector('.input2')
+const nextBtn=document.querySelector('#check-next');
+
+const hideTable=document.querySelector('table')
+inputTwo.style.display="none"
+checkButton.style.display="none"
+hideTable.style.display="none"
+
 
 const numberOfNotes=[2000,500,100,50,10,5,1];
 function calculateBill() {
     errorMessage.style.display="none"
-
+   
     if(billAmount.value >0){
-        if(Number(givenAmount.value) >= Number(billAmount.value)){
+        
+        if(givenAmount.value >0){
+            
+            if(Number(givenAmount.value) >= Number(billAmount.value)){     
             const subtractValue= givenAmount.value - billAmount.value;
             console.log(subtractValue);
+                
             calculateReturn(subtractValue);
             errorMessage.style.display="block"
             errorMessage.innerText="Amount should be return " + subtractValue;
+            hideTable.style.display="inline-block"
         }else{
             errorMessage.style.display="block"
         errorMessage.innerText="Hey!!!! You have to pay the full amount"
-
+        hideTable.style.display="none"
         }
     }else{
         errorMessage.style.display="block"
+        errorMessage.innerText="Given amount must be greater than 0"
+        hideTable.style.display="none"
+    }
+
+    }else{
+        errorMessage.style.display="block"
         errorMessage.innerText="Bill amount must be greater than 0"
+        hideTable.style.display="none"
     }
 }
 function calculateReturn(subtractValue) {
@@ -39,4 +59,18 @@ function calculateReturn(subtractValue) {
     //    console.log(remainNumber);
    }
 }
+nextBtn.addEventListener('click',function(){
+    if(billAmount.value === ''){
+        errorMessage.style.display="block"
+        errorMessage.innerText="Bill amount should not be empty" 
+    }else if(billAmount.value <= 0){
+        errorMessage.style.display="block"
+        errorMessage.innerText="Bill amount must be greater than zero" 
+    }
+    else{
+        nextBtn.style.display="none"
+        inputTwo.style.display="block"
+        checkButton.style.display="block"
+    }
+})
 checkButton.addEventListener('click',calculateBill);
